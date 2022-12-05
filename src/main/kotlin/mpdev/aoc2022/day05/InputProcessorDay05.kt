90ed38b14f00481778ee7c45b878da9e065ce91b
marinos <marinosch.99981@gmail.com>
2022-12-05 18:05:06 +0100

    refactored day 5 to use Regex

--- src/main/kotlin/mpdev/aoc2022/day05/InputProcessorDay05.kt
+++ src/main/kotlin/mpdev/aoc2022/day05/InputProcessorDay05.kt
@@ -2,10 +2,11 @@ package mpdev.aoc2022.day05
 
 import mpdev.aoc2022.common.InputProcessor
 import mpdev.aoc2022.common.abort
+import java.lang.Exception
 
 class InputProcessorDay05: InputProcessor<InputDay05>() {
 
-    private fun processLine1(inputLine: String, dataList: MutableList<String>, numStacks: Int) {
+    private fun processStack(inputLine: String, dataList: MutableList<String>, numStacks: Int) {
         for (i in 1..numStacks) {
             val index = (i - 1) * 4 + 1
             if (index < inputLine.length)
@@ -14,29 +15,31 @@ class InputProcessorDay05: InputProcessor<InputDay05>() {
         }
     }
 
-    private fun processLine2(inputLine: String, dataList: MutableList<Move>) {
-        val split = inputLine.split(" ")
-        if (split.size != 6)
-            abort("bad input line $inputLine")
-        dataList.add(Move(split[1].toInt(), split[3].toInt(), split[5].toInt()))
+    private fun processMove(inputLine: String, dataList: MutableList<Move>) {
+        val matches = """move (\d+) from (\d+) to (\d+)""".toRegex().find(inputLine)
+        try {
+            val (cnt, from, to) = matches!!.destructured
+            dataList.add(Move(cnt.toInt(), from.toInt(), to.toInt()))
+        }
+        catch (e: Exception) { abort("bad input line $inputLine") }
     }
 
     override fun process(input: List<String>): InputDay05 {
         val stacks = mutableListOf<String>()
         val moves = mutableListOf<Move>()
 
-        val numStacks = input.stream().filter { it.matches(Regex("^[0-9\\s]+\$")) }
+        val numStacks = input.stream().filter { it.matches(Regex("^[0-9+\\s*]+\$")) }
             .toList()[0].split(" ").last().toInt()
         for (i in 1..numStacks)
             stacks.add("")
         var inputPart1 = true
         input.forEach {
             if (inputPart1) {
-                processLine1(it, stacks, numStacks)
+                processStack(it, stacks, numStacks)
                 if (it.isEmpty())
                     inputPart1 = false
             } else
-                processLine2(it, moves)
+                processMove(it, moves)
         }
         for (i in 0 until stacks.size) {
             stacks[i] = stacks[i].reversed()
