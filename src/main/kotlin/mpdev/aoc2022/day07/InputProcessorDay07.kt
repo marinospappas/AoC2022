62cd960833a2f461ede1d19d09688d83b4829c74
marinos <marinosch.99981@gmail.com>
2022-12-07 15:20:48 +0100

    Day 7 upd 6 refactored input processor

--- src/main/kotlin/mpdev/aoc2022/day07/InputProcessorDay07.kt
+++ src/main/kotlin/mpdev/aoc2022/day07/InputProcessorDay07.kt
@@ -2,39 +2,34 @@ package mpdev.aoc2022.day07
 
 import mpdev.aoc2022.common.InputProcessor
 
+var regexMatch: MatchResult? = null
+fun String.matchRegExp(regexp: String) = regexp.toRegex().find(this).also { regexMatch = it } != null
+
 class InputProcessorDay07: InputProcessor<InputDay07>() {
 
     override fun process(input: List<String>): InputDay07 {
         val rootDir = ADirectoryEntry("/", "dir")
-        input.forEach {
+
+        input.forEach { line ->
             when {
-                it.matches(Regex("""^\$ cd /$""")) -> rootDir.makeRootCurrent()
-                it.matches(Regex("""^\$ ls$""")) -> Unit
-                it.matches(Regex("""^\$ cd [a-zA-Z].*$""")) -> changeCurrentDir(it, rootDir)
-                it.matches(Regex("""^\$ cd \.\.$""")) -> rootDir.makeParentCurrent()
-                it.matches(Regex("""^dir [a-zA-Z].*$""")) -> createDir(it, rootDir)
-                it.matches(Regex("""^[0-9]+ [a-zA-Z].*$""")) -> createFile(it, rootDir)
+                line.matchRegExp("""^\$ ls$""") -> {}
+                line.matchRegExp("""^\$ cd /$""") -> rootDir.makeRootCurrent()
+                line.matchRegExp("""^\$ cd \.\.$""") -> rootDir.makeParentCurrent()
+                line.matchRegExp("""cd ([a-zA-Z\d.\-]+)""") -> {
+                    val (dirName) = regexMatch!!.destructured
+                    rootDir.changeCurDir(dirName)
+                }
+                line.matchRegExp("""dir ([a-zA-Z\d.\-]+)""") -> {
+                    val (dirName) = regexMatch!!.destructured
+                    rootDir.createDir(dirName)
+                }
+                line.matchRegExp("""(\d+) ([a-zA-Z\d.\-]+)""") -> {
+                    val (fileSize, fileName) = regexMatch!!.destructured
+                    rootDir.createFile(fileName, fileSize.toInt())
+                }
             }
         }
         rootDir.updateDirSizes()
         return InputDay07(rootDir)
-    }
-
-    private fun changeCurrentDir(line: String, rootDir: ADirectoryEntry) {
-        val match = Regex("""cd ([a-zA-Z\d.\-]+)""").find(line)
-        val (dirName) = match!!.destructured
-        rootDir.changeCurDir(dirName)
-    }
-
-    private fun createDir(line: String, rootDir: ADirectoryEntry) {
-        val match = Regex("""dir ([a-zA-Z\d.\-]+)""").find(line)
-        val (dirName) = match!!.destructured
-        rootDir.createDir(dirName)
-    }
-
-    private fun createFile(line: String, rootDir: ADirectoryEntry) {
-        val match = Regex("""(\d+) ([a-zA-Z\d.\-]+)""").find(line)
-        val (fileSize, fileName) = match!!.destructured
-        rootDir.createFile(fileName, fileSize.toInt())
     }
 }
