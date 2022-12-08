e35a11e586593ac97fbb336c0408864523de1e8e
marinos <marinosch.99981@gmail.com>
2022-12-08 20:09:36 +0100

    day 9 upd 0

--- src/main/kotlin/mpdev/aoc2022/day08/InputProcessorDay08.kt
+++ src/main/kotlin/mpdev/aoc2022/day08/InputProcessorDay08.kt
@@ -7,15 +7,11 @@ class InputProcessorDay08: InputProcessor<InputDay08>() {
     var regexMatch: MatchResult? = null
     private fun String.matchRegExp(regex: Regex) = regex.find(this).also { regexMatch = it } != null
 
-    private fun processLine(line: String, treeGrid: MutableList<List<Int>>) {
-        val row = mutableListOf<Int>()
-        line.forEach { char -> row.add(char.digitToInt()) }
-        treeGrid.add(row)
-    }
+    private fun processLine(line: String, treeGrid: MutableList<List<Int>>) =
+        treeGrid.add(mutableListOf<Int>().also { line.forEach { char -> it.add(char.digitToInt()) } })
 
     override fun process(input: List<String>): InputDay08 {
         val treeGrid: MutableList<List<Int>> = mutableListOf()
-
         input.forEach { line -> processLine(line, treeGrid)}
         return InputDay08(treeGrid, treeGrid.size, treeGrid[0].size)
     }
--- src/main/kotlin/mpdev/aoc2022/day09/InputProcessorDay09.kt
+++ src/main/kotlin/mpdev/aoc2022/day09/InputProcessorDay09.kt
@@ -7,16 +7,12 @@ class InputProcessorDay09: InputProcessor<InputDay09>() {
     var regexMatch: MatchResult? = null
     private fun String.matchRegExp(regex: Regex) = regex.find(this).also { regexMatch = it } != null
 
-    private fun processLine(line: String, treeGrid: MutableList<List<Int>>) {
-        val row = mutableListOf<Int>()
-        line.forEach { char -> row.add(char.digitToInt()) }
-        treeGrid.add(row)
-    }
+    private fun processLine(line: String, dataList: MutableList<List<Int>>) =
+        dataList.add(mutableListOf<Int>().also { line.forEach { char -> it.add(char.digitToInt()) } })
 
     override fun process(input: List<String>): InputDay09 {
-        val treeGrid: MutableList<List<Int>> = mutableListOf()
-
-        input.forEach { line -> processLine(line, treeGrid)}
-        return InputDay09(treeGrid, treeGrid.size, treeGrid[0].size)
+        val dataList: MutableList<List<Int>> = mutableListOf()
+        input.forEach { line -> processLine(line, dataList)}
+        return InputDay09(dataList, dataList.size, dataList[0].size)
     }
 }
