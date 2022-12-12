8ef8dd5ccc83b2ac0db32c0c97b8ee88de0bf471
marinos <marinosch.99981@gmail.com>
2022-12-12 18:26:12 +0100

    day 12 upd 2

--- src/main/kotlin/mpdev/aoc2022/day12/SolutionProcessorDay12.kt
+++ src/main/kotlin/mpdev/aoc2022/day12/SolutionProcessorDay12.kt
@@ -3,21 +3,22 @@ package mpdev.aoc2022.day12
 import mpdev.aoc2022.common.*
 import mpdev.aoc2022.utils.AStar
 import mpdev.aoc2022.utils.Dijkstra
+import mpdev.aoc2022.utils.ConsoleColour
 
 class SolutionProcessorDay12: SolutionProcessor<InputDay12> {
 
     /** part 1 calculation */
     override fun part1(input: InputDay12): String {
         input.graphData = input.createGraph('U')
-        //val minPath = Dijkstra<Pair<Int,Int>>(input.graphData.costs)
-        val minPath = AStar<Pair<Int,Int>>(input.graphData.costs, input.graphData.heuristics)
+        val minPath = Dijkstra<Pair<Int,Int>>(input.graphData.costs)
+        //val minPath = AStar<Pair<Int,Int>>(input.graphData.costs, input.graphData.heuristics)
         val res = minPath.runIt(input.graphData[input.startId], input.graphData[input.endId])
         for (i in 0..input.maxY) {
             for (j in 0..input.maxX)
                 if (res.path.contains(Pair(j,i)))
-                    print(input.heightList[i][j])
+                    print(ConsoleColour.RED + input.heightList[i][j].uppercaseChar() + ConsoleColour.RESET)
                 else
-                    print(".")
+                    print(input.heightList[i][j])
             println()
         }
         return (res.path.size - 1).toString()
